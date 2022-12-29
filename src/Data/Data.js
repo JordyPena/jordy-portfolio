c7090d165252942c5c569933c211572a9296e4ec
Jordy Pena <jordypenamontes@gmail.com>
2022-12-29 16:28:12 -0600

    projects component added and data created

---
+++ src/Data/Data.js
@@ -0,0 +1,22 @@
+const Data = [
+  {
+    id: 1,
+    title: 'Pokemon TCG Guru',
+    description: `A Platform That Helps You Discover The Value Of Your Pokemon
+    Cards, And Find And Purchase New Cards With Updated Current And
+    Previous Pricing.`,
+    framework: 'React',
+    url: 'https://tcg.vercel.app/',
+    img: 'https://i.imgur.com/xVanUSo.jpg'
+  },
+  {
+    id: 2,
+    title: 'Straub Construction',
+    description: `Personal construction website focusing on displaying previous body of work, dynamic pricing and summaries, along with pinch and zoom blueprints.`,
+    framework: 'React',
+    url: 'https://construction-freelance.vercel.app/',
+    img: 'https://i.imgur.com/RzTpK3L.jpg'
+  }
+];
+
+export default Data;
--- src/Pages/Projects/projects.jsx
+++ src/Pages/Projects/projects.jsx
@@ -1,9 +1,17 @@
 import Navigation from "../../Components/Navigation/navigation";
 import Footer from "../../Components/Footer/footer";
+import ProjectCard from "../../Components/ProjectCard/ProjectCard";
+import Data from '../../Data/Data';
+
 const Projects = () => {
   return (
     <>
       <Navigation />
+      {Data.map((project) => {
+        return (
+          <ProjectCard project={project} key={project.id} />
+        )
+      })}
       <Footer />
     </>
   )
