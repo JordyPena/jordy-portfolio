b75758db69d53267edc344e2089f012e1aaf1c6e
Jordy Pena <67558072+JordyPena@users.noreply.github.com>
2022-12-29 21:03:57 -0600

    mobile responsive added (#4)

--- src/Components/Navigation/navigation.jsx
+++ src/Components/Navigation/navigation.jsx
@@ -1,32 +1,66 @@
-import SideNav from '../SideNav/sideNav';
-import { CgMenuRightAlt, CgClose } from 'react-icons/cg'
-import { RiAliensFill } from 'react-icons/ri';
-import '../Navigation/navigation.css';
-import { useState } from 'react';
+import SideNav from "../SideNav/sideNav";
+import { CgMenuRightAlt, CgClose } from "react-icons/cg";
+import { RiAliensFill } from "react-icons/ri";
+import "../Navigation/navigation.css";
+import { useState, useEffect } from "react";
 
 const Navigation = () => {
   const [openMenu, setOpenMenu] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [width, setWidth] = useState({
+    dynamicWidth: window.innerWidth,
+  });
+
+  const setDimension = () => {
+    setWidth({
+      dynamicWidth: window.innerWidth,
+    });
+  };
+
+  //Grabs window width and calls setDimension method to
+  //Set our width state to dynamically keep track of screenSize
+  useEffect(() => {
+    window.addEventListener("resize", setDimension);
+    return () => window.removeEventListener("resize", setDimension);
+  }, []);
+
+  //Keeps track of the width state to dynamically update our isMobile state
+  //To conditionally show/hide elements
+  useEffect(() => {
+    return width.dynamicWidth < 600 ? setIsMobile(true) : setIsMobile(false);
+  }, [width]);
 
   const menuSelected = () => {
-    setOpenMenu(!openMenu)    
-  }
+    setOpenMenu(!openMenu);
+  };
   return (
-    <nav>
-      <div className='icon-container'>
-        <RiAliensFill color='black' size='4rem' />
-        {openMenu ?
-          <button className='menu-btn' onClick={() => menuSelected()}>
-            <CgClose color='black' size='4rem' />
-          </button>
-           :
-          <button className='menu-btn' onClick={() => menuSelected()}>
-            <CgMenuRightAlt color='black' size='4rem' />
-          </button>
-        }
-      </div>
-      {openMenu && <SideNav/>}
-    </nav>
-  )
-}
+    <>
+      {isMobile ? (
+        <nav>
+          <div className="icon-container">
+            <RiAliensFill color="black" size="4rem" />
+            {openMenu ? (
+              <button className="menu-btn" onClick={() => menuSelected()}>
+                <CgClose color="black" size="4rem" />
+              </button>
+            ) : (
+              <button className="menu-btn" onClick={() => menuSelected()}>
+                <CgMenuRightAlt color="black" size="4rem" />
+              </button>
+            )}
+          </div>
+          {openMenu && <SideNav />}
+        </nav>
+      ) : (
+        <nav>
+          <div className="icon-container">
+            <RiAliensFill color="black" size="4rem" />
+          </div>
+          <SideNav />
+        </nav>
+      )}
+    </>
+  );
+};
 
 export default Navigation;
